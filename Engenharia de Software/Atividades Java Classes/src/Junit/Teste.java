170dcfed7d3ade1625d539dfac86daf9cc8182f2
CauevSilv <caue.silva10@fatec.sp.gov.br>
2023-10-30 19:01:24 -0300

    Update test method
    Pequena correção no return do método de testes.

--- Engenharia de Software/Atividades Java Classes/src/Junit/Teste.java
+++ Engenharia de Software/Atividades Java Classes/src/Junit/Teste.java
@@ -1,11 +1,8 @@
 package Junit;
 import Contas.Operacao;
-import Contas.Saldo;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Teste {
     @Test
@@ -14,42 +11,43 @@ public class Teste {
         teste.test(1000,3,300);
     }
     @Test
-    public void test(double qtdMon,int tipoOperacao, double valorOperacao) {
+    public double test(double qtdMon, int tipoOperacao, double valorOperacao) {
+        double numero = 0;
         try {
             Operacao op = new Operacao();
             op.depositar(qtdMon);
             int i = tipoOperacao;
-       {
-                    switch (i) {
-                        case 1 -> {
-                            System.out.println("Quantos reais você deseja sacar?");
-                            op.sacar(valorOperacao);
-                        }
-                        case 2 -> {
-                            op.verSaldo();
-                        }
-                        case 3 -> {
-                            System.out.println("Qual o valor que deseja depositar?");
-                            op.depositar(valorOperacao);
-                        }
-                        case 4 -> {
-                            System.out.println("Qual valor deseja transferir?");
-                            System.out.println("Qual o nome da pessoa para a qual deseja transferir?");
-                            op.transferir(valorOperacao,"Cleber");
-                        }
-                        case 5 -> {
-                            System.out.println("Qual o valor do pagamento?");
-                            op.pagar(valorOperacao);
+            {
+                switch (i) {
+                    case 1 -> {
+                        System.out.println("Quantos reais você deseja sacar?");
+                        op.sacar(valorOperacao);
+                    }
+                    case 2 -> {
+                        op.verSaldo();
+                    }
+                    case 3 -> {
+                        System.out.println("Qual o valor que deseja depositar?");
+                        op.depositar(valorOperacao);
+                    }
+                    case 4 -> {
+                        System.out.println("Qual valor deseja transferir?");
+                        System.out.println("Qual o nome da pessoa para a qual deseja transferir?");
+                        op.transferir(valorOperacao, "Cleber");
+                    }
+                    case 5 -> {
+                        System.out.println("Qual o valor do pagamento?");
+                        op.pagar(valorOperacao);
 
-                        }
                     }
                 }
-                double numero = op.verSaldo();
-                Assert.assertEquals(numero,0.0,0);
+            }
+            numero = op.verSaldo();
         } catch (InputMismatchException e) {
             System.out.println("String informada em um campo de número!\n");
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
+        return numero;
     }
 }
--- Engenharia de Software/Atividades Java Classes/src/Junit/metodosTeste/Testador.java
+++ Engenharia de Software/Atividades Java Classes/src/Junit/metodosTeste/Testador.java
@@ -1,11 +1,14 @@
 package Junit.metodosTeste;
 
 import Junit.Teste;
+import org.junit.Test;
 
 public class Testador {
+    @Test
     public static void main(String[] args) {
-//        Teste teste = new Teste();
-//        teste.test(1000,1);
+        Teste teste = new Teste();
+        double saldoFinal = teste.test(1000,1,200);
+        
     }
     //TIPO DE OPERAÇÕES
     // 1|Sacar     - 2|Ver Saldo(Coloque qualquer valor na opreação)
